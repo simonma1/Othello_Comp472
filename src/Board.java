35dee3fa65c2b89d83f0ca62e725e7ff8bdf97bf
Simon Moniere Abes <simon.moniereabes@gmail.com>
2016-11-07 15:38:07 -0500

    Modified board

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,33 @@
+import java.awt.*;
+import java.util.HashMap;
+
 /**
  * Created by Simon on 2016-11-07.
  */
 public class Board {
+
+    private Player playerOne;
+    private Player playerTwo;
+
+
+    private final int BOARD_WIDTH = 8;
+    private final int BOARD_HEIGHT = 8;
+
+
+
+   // private HashMap<Point, > board = new HashMap();
+
+
+    public Board(Player p1){
+        playerOne = p1;
+    }
+
+    public Board(Player p1, Player p2){
+        playerOne = p1;
+        playerTwo = p2;
+    }
+
+    public void startNewGame(){
+
+    }
 }
