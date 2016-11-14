de673b9b06ddc09b98bc77f2f5dd37b4e595faae
Simon Moniere Abes <simon.moniereabes@gmail.com>
2016-11-14 14:42:41 -0500

    Harisson is a double idiot. Made me forgot to stage the constant class

--- src/Constant.java
+++ src/Constant.java
@@ -1,7 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Simon on 2016-11-14.
  */
-public class Constant {
+public  class Constant {
+
+    public static final int MAXBETAVALUE = 1000000;
+    public static final int MINALPHAVALUE = -1000000;
+
 }
