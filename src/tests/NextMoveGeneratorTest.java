a183f2964fee3804561aa4fa868bac30e63cbf40
Simon Moniere Abes <simon.moniereabes@gmail.com>
2016-11-23 15:03:21 -0500

    Added JUnit tests

---
+++ src/tests/NextMoveGeneratorTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Simon on 2016-11-23.
+ */
+public class NextMoveGeneratorTest {
+
+    @Test
+    public void generateNextStates() throws Exception {
+        
+        assertTrue(true);
+    }
+
+}
