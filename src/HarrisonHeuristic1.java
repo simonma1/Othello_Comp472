3437567e1a426cbbf4305118e8ea02b5c82fdce8
Harrison Ianatchkov <harrison1993@gmail.com>
2016-11-25 11:17:32 -0500

    Implemented the absolute best heuristic there ever was. No one could match the greatness of this commit, not even the one, the only, Othello. Fixed the stupid author name... what do you think this is?

--- src/HarrisonHeuristic1.java
+++ src/HarrisonHeuristic1.java
@@ -2,13 +2,88 @@ import java.awt.*;
 import java.util.HashMap;
 
 /**
- * Created by BaDaS on 2016-11-24.
+ * Created by Harrison on 2016-11-24.
  */
 public class HarrisonHeuristic1 implements HeuristicCalculator {
+    private final int MIDDLE = 1;
+    private final int MIDDLE_SHELL = 10;
+    private final int OUTER_SHELL = 100;
+    private final int BORDER_SHELL = 1000;
+    private final int CORNER = 2000;
+    private SquareState currentPlayerColour;
+    private SquareState opponentPlayerColour;
+    private int heuristicScore = 0;
 
     @Override
     public int calculateHeuristic(Board board, Turn turn) {
-        return 0;
+        heuristicScore = 0;
+
+        // calculate
+        for (Point whitePiece : board.getWhitePieces()) {
+            if (isMiddlePiece(whitePiece))
+                updateHeuristic(-MIDDLE);
+            else if (isMiddleShellPiece(whitePiece))
+                updateHeuristic(-MIDDLE_SHELL);
+            else if (isOuterShellPiece(whitePiece))
+                updateHeuristic(-OUTER_SHELL);
+            else if (isBorderShellPiece(whitePiece))
+                updateHeuristic(-BORDER_SHELL);
+            else if (isCornerPiece(whitePiece))
+                updateHeuristic(-CORNER);
+        }
+
+        for (Point blackPiece : board.getBlackPieces()) {
+            if (isMiddlePiece(blackPiece))
+                updateHeuristic(MIDDLE);
+            else if (isMiddleShellPiece(blackPiece))
+                updateHeuristic(MIDDLE_SHELL);
+            else if (isOuterShellPiece(blackPiece))
+                updateHeuristic(OUTER_SHELL);
+            else if (isBorderShellPiece(blackPiece))
+                updateHeuristic(BORDER_SHELL);
+            else if (isCornerPiece(blackPiece))
+                updateHeuristic(CORNER);
+        }
+        return heuristicScore;
+    }
+
+    private boolean isCornerPiece(Point piece) {
+        return (piece.getX() == 0 && piece.getY() == 0) ||
+                (piece.getX() == 0 && piece.getY() + 1 == Board.BOARD_WIDTH) ||
+                (piece.getX() + 1 == Board.BOARD_HEIGHT && piece.getY() == 0) ||
+                (piece.getX() + 1 == Board.BOARD_HEIGHT && piece.getY() + 1 == Board.BOARD_WIDTH);
+    }
+
+    private boolean isBorderShellPiece(Point piece) {
+        return (piece.getX() == 0 && piece.getY() + 1 != Board.BOARD_WIDTH) ||
+                (piece.getX() != 0 && piece.getY() + 1 == Board.BOARD_WIDTH) ||
+                (piece.getX() + 1 == Board.BOARD_HEIGHT && piece.getY() != 0) ||
+                (piece.getX() + 1 != Board.BOARD_HEIGHT && piece.getY() == 0);
+    }
+
+    private boolean isOuterShellPiece(Point piece) {
+        return piece.getX() - 1 == 0 ||
+                piece.getX() + 2 == Board.BOARD_HEIGHT ||
+                piece.getY() - 1 == 0 ||
+                piece.getY() + 2 == Board.BOARD_WIDTH;
+    }
+
+    private boolean isMiddleShellPiece(Point piece) {
+       return piece.getX() - 2 == 0 ||
+               piece.getX() + 3 == Board.BOARD_HEIGHT ||
+               piece.getY() - 2 == 0 ||
+               piece.getY() + 3 == Board.BOARD_WIDTH;
+    }
+
+    private boolean isMiddlePiece(Point piece) {
+        return piece.getX() - 3 == 0 ||
+                piece.getX() + 4 == Board.BOARD_HEIGHT ||
+                piece.getY() - 3 == 0 ||
+                piece.getY() + 4 == Board.BOARD_WIDTH;
+    }
+
+    private void updateHeuristic(int points) {
+        heuristicScore += points;
     }
 
     @Override
